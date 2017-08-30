ff4c9ee87cf25889f1b17b34a37f68bcf43a6b46
aJchemist <valchemist@me.com>
2017-08-30 12:56:58 +0900

    Revised travis ci automation

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+self="${BASH_SOURCE[0]}"
+selfdir="$(cd "$(dirname "${self}")"; pwd)"
+selfpath="$selfdir/$(basename "$self")"
+
+function docker_build()
+{
+    cd "${RDIR}/${TAG}${VARIANT:+/$VARIANT}"
+
+    REPO="${DOCKER_HUB_USERNAME}/${RDIR}"
+
+    docker build -t $REPO:$COMMIT .
+    docker tag $REPO:$COMMIT $REPO:travis${TRAVIS_BUILD_NUMBER:+-$TRAVIS_BUILD_NUMBER}
+    docker tag $REPO:$COMMIT $REPO:$TAG${VARIANT:+-$VARIANT}
+    for _tag in ${TAGS[@]}
+    do
+        docker tag $REPO:$COMMIT $REPO:$_tag${VARIANT:+-$VARIANT}
+    done
+    if [ ! -z $LATEST ]; then
+        LATEST_TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo latest; else echo $TRAVIS_BRANCH; fi)
+        docker tag $REPO:$COMMIT $REPO:$LATEST_TAG${VARIANT:+-$VARIANT}
+    fi
+}
+
+function docker_push()
+{
+    docker push $REPO
+}
+
+case $RDIR in
+    alpine)
+        docker_build
+        VARIANT=openssh docker_build
+        ;;
+    *)
+        docker_build
+        ;;
+esac
+
+docker_push
