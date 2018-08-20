6ecfb26dcd8fb17096f00c8541e6a82cc13e2b26
aJchemist <valchemist@me.com>
2018-08-20 22:40:04 +0900

    Revised script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+
 self="${BASH_SOURCE[0]}"
 selfdir="$(cd "$(dirname "${self}")"; pwd)"
 selfpath="$selfdir/$(basename "$self")"
+
 
 #
 # $TRAVIS_BUILD_NUMBER
 # $TRAVIS_JOB_ID
 # $TRAVIS_JOB_NUMBER
 
+
 function docker_build()
 {
-    cd "${ADIR}/${TAG}${VARIANT:+/$VARIANT}"
+    DOCKERFILE_PATH=${DOCKERFILE_PATH:-"${ADIR}/${TAG}${VARIANT:+/$VARIANT}"}
+    pushd "$DOCKERFILE_PATH"
 
     REPO="${DOCKER_HUB_USERNAME}/${ADIR}"
 
     IMAGE="$REPO:travis${COMMIT:+-$COMMIT}${TRAVIS_JOB_NUMBER:+-$TRAVIS_JOB_NUMBER}"
 
     docker build -t $IMAGE${BUILD_ARGS:+ $BUILD_ARGS} .
+    popd
 
     # tagging
     docker tag $IMAGE $REPO:${TAG_PREFIX:+$TAG_PREFIX-}$TAG${VARIANT:+-$VARIANT}${TAG_SUFFIX:+-$TAG_SUFFIX}
@@ -35,12 +40,15 @@ function docker_build()
     fi
 }
 
+
 function docker_push()
 {
     docker push $REPO
 }
 
+
 # * build
+
 
 echo "******"
 echo "******" building...
@@ -53,16 +61,20 @@ case $ADIR in
         ;;
 esac
 
+
 if [ ! $? -eq 0 ]; then
     echo FAILED ON BUILD STAGE
     exit 1;
 fi
 
+
 # * test
+
 
 echo "******"
 echo "******" testing...
 echo "******"
+
 
 case $ADIR in
     oraclejdk)
@@ -81,11 +93,14 @@ if [ ! $? -eq 0 ]; then
     exit 1;
 fi
 
+
 # * push
+
 
 echo "******"
 echo "******" pushing...
 echo "******"
+
 
 case $ADIR in
     *)
