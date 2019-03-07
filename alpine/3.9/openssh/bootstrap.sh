e2e5f4c6d019ccd8b5dee7c6b7cbbdb3ebce92a7
aJchemist <1694505+aJchemist@users.noreply.github.com>
2019-03-07 18:13:54 +0900

    Added alpine:3.9

---
+++ alpine/3.9/openssh/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "$DEBUG" == 'true' ] && set -x
+
+chmod +x /openssh_entry.sh
+
+# Generate Host keys, if required
+if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+    ssh-keygen -A
+fi
